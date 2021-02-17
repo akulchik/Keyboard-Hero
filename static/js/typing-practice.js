e8f5fdd5f364c9b50639b805b0469d30bebb3fb9
Andrei Kulchik <andrei.kulchik@gmail.com>
2021-02-17 18:44:59 +0300

    iterate over 'span' elements to track typing progress

--- static/js/typing-practice.js
+++ static/js/typing-practice.js
@@ -2,12 +2,27 @@
 
 const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla lacus, suscipit et placerat a, tempus ac dolor. Donec auctor, ligula id viverra consequat, nunc tellus sagittis metus, vitae tincidunt ante nisl vel tellus. Vestibulum id nibh commodo, sodales risus vitae, gravida orci.'
 
-function mount() {
-  mountTypingText()
-  document.addEventListener('keyup', KeyUpListener(text))
+function KeyUpListener(letterElements) {
+  const letters = lettersGenerator(letterElements)
+  return function (e) {
+    const { done, value: el } = letters.next()
+    if (done) return
+    console.log(`released ${e.key} when required ${el.textContent}`)
+  }
 }
 
-function mountTypingText() {
+function* lettersGenerator(letterElements) {
+  for (const el of letterElements) {
+    yield el
+  }
+}
+
+function mount(text) {
+  const mountedText = mountTypingText(text)
+  document.addEventListener('keyup', KeyUpListener(mountedText))
+}
+
+function mountTypingText(text) {
   const typingField = document.querySelector('#kbHeroTypingField')
   const typingText = document.createElement('div')
   for (const letter of text) {
@@ -17,23 +32,10 @@ function mountTypingText() {
     typingText.appendChild(span)
   }
   typingField.appendChild(typingText)
-}
-
-function* lettersGenerator(text) {
-  for (const letter of text) {
-    yield letter
-  }
-}
-
-function KeyUpListener(text) {
-  const letters = lettersGenerator(text)
-  return function (e) {
-    const { done, value } = letters.next()
-    if (done) return
-    console.log(`released ${e.key} when required ${value}`)
-  }
+  return (document.querySelector('#kbHeroTypingField')
+    .querySelectorAll('span.kb-hero-typing-field__letter'))
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  mount()
+  mount(text)
 })
